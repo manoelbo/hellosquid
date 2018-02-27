2aa8f1dd6b4e715d0620556d157019bb904367c4
manoelbo <manoelbo@hotmail.com>
2018-02-27 20:36:41 -0300

    fix: react path conflit

--- app/index.js
+++ app/index.js
@@ -5,36 +5,33 @@ const app = express();
 require('./api/models/db');
 
 const apiRouter = require('./api/routes/api');
-
-
 app.use('/api/v1/', apiRouter);
 
 app.get('/teste', (req, res) => {
     res.send({ hi: 'there' });
 });
 
-app.get('/', (req, res) => {
-    res.redirect('/app')
-})
+// app.get('/', (req, res) => {
+//     res.redirect('/app/');
+// });
 
-app.get('', (req, res) => {
-    res.redirect('/app')
-})
+
+
+
 
 if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
     // Express will serve up production assets
     // like our main.js file, or main.css file!
-    app.use('/app', express.static('app/client/build'));
+    app.use(express.static(path.resolve(__dirname, '../app/client/build')));
 
     // Express will serve up the index.html file
     // if it doesn't recognize the route
-    const path = require('path');
-    app.get('*/app/*', (req, res) => {
+
+    app.get('/app/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
-
-
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
