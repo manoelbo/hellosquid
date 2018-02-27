96b1d61429cc50b80e7a852a571d185ec8200d11
manoelbo <manoelbo@hotmail.com>
2018-02-27 20:23:38 -0300

    fix: react path

--- app/index.js
+++ app/index.js
@@ -7,6 +7,12 @@ require('./api/models/db');
 const apiRouter = require('./api/routes/api');
 
 
+app.use('/api/v1/', apiRouter);
+
+app.get('/teste', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
 app.get('/', (req, res) => {
     res.redirect('/app')
 })
@@ -23,16 +29,12 @@ if (process.env.NODE_ENV === 'production') {
     // Express will serve up the index.html file
     // if it doesn't recognize the route
     const path = require('path');
-    app.get('/app/*', (req, res) => {
+    app.get('/app*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
-app.use('/api/v1/', apiRouter);
 
-app.get('/teste', (req, res) => {
-    res.send({ hi: 'there' });
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
