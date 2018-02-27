0b009ca75ad88834efb99de1bd2e575d6e84ff60
manoelbo <manoelbo@hotmail.com>
2018-02-27 11:27:52 -0300

    fix: react path

--- app/index.js
+++ app/index.js
@@ -11,12 +11,10 @@ app.get('/teste', (req, res) => {
     res.send({ hi: 'there' });
 });
 
-console.log(__dirname, 'dir');
-
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
+  app.use(express.static('app/client/build'));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
