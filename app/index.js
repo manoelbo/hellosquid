4d56b251bbe753b29152c84754bce2e3e97205bd
manoelbo <manoelbo@hotmail.com>
2018-02-27 20:26:57 -0300

    fix: react path

--- app/index.js
+++ app/index.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
     // Express will serve up the index.html file
     // if it doesn't recognize the route
     const path = require('path');
-    app.get('/app*', (req, res) => {
+    app.get('*/app/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
