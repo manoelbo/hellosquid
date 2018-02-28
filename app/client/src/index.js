d9d932d8a4b399044d768fdb27343a111d4951d5
manoelbo <manoelbo@hotmail.com>
2018-02-27 22:13:42 -0300

    fix: unregister react service workerr

--- app/client/src/index.js
+++ app/client/src/index.js
@@ -3,12 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
+import { unregister } from './registerServiceWorker';
 
 ReactDOM.render(
     <BrowserRouter>
         <App />
-    </BrowserRouter>, 
+    </BrowserRouter>,
     document.getElementById('root')
 );
-registerServiceWorker();
+
+unregister();
