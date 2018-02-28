482ffe51fbedecdc535eac19be8f9c4ddde25a5d
manoelbo <manoelbo@hotmail.com>
2018-02-27 22:03:53 -0300

    remove register service wroker

--- app/index.js
+++ app/index.js
@@ -1,13 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
 
 require('./api/models/db');
+
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, '../app/client/build')));
 
 const apiRouter = require('./api/routes/api');
 app.use('/api/v1/', apiRouter);
 
-app.get('/teste', (req, res) => {
+app.get('*/teste', (req, res) => {
     res.send({ hi: 'there' });
 });
 
@@ -15,23 +19,11 @@ app.get('/teste', (req, res) => {
 //     res.redirect('/app/');
 // });
 
+//
+// app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+// });
 
-
-
-
-if (process.env.NODE_ENV === 'production') {
-    const path = require('path');
-    // Express will serve up production assets
-    // like our main.js file, or main.css file!
-    app.use(express.static(path.resolve(__dirname, '../app/client/build')));
-
-    // Express will serve up the index.html file
-    // if it doesn't recognize the route
-
-    app.get('/app/*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
